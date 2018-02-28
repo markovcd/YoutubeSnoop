b16e02446e6b964d396fedb39fd9dd9c23201ab4
Arkadiusz Markowski <arkadiusz.markowski@avid.com>
2018-02-28 08:16:09 +0100

    Formatted ResourceKind

--- Source/Enums/ResourceKind.cs
+++ Source/Enums/ResourceKind.cs
@@ -5,35 +5,50 @@ namespace YoutubeSnoop.Enums
     public enum ResourceKind
     {
         None,
-        Activity,
-        ActivityListResponse,
-        Caption,
-        CaptionListResponse,
-        [ApiResourceMapping(typeof(Entities.Channel))]      Channel,
-        [ApiResponseMapping(typeof(Entities.Channel))]      ChannelListResponse,
-        ChannelSection,
-        ChannelSectionListResponse,
-        Comment,
-        CommentListResponse,
-        CommentThread,
-        CommentThreadListResponse,
-        GuideCategory,
-        GuideCategoryListResponse,
-        I18nLanguage,
-        I18nLanguageListResponse,
-        I18nRegion,
-        I18nRegionListResponse,
-        [ApiResourceMapping(typeof(Entities.Playlist))]     Playlist,
-        [ApiResourceMapping(typeof(Entities.PlaylistItem))] PlaylistItem,
-        [ApiResponseMapping(typeof(Entities.PlaylistItem))] PlaylistItemListResponse,
-        [ApiResponseMapping(typeof(Entities.PlaylistItem))] PlaylistListResponse,
-        [ApiResourceMapping(typeof(Entities.SearchResult))] SearchResult,
-        [ApiResponseMapping(typeof(Entities.SearchResult))] SearchListResponse,
-        Subscription,
-        SubscriptionListResponse,
-        [ApiResourceMapping(typeof(Entities.Video))]        Video,
-        VideoCategory,
-        videoCategoryListResponse,
-        [ApiResponseMapping(typeof(Entities.Video))]        VideoListResponse,       
+        /*[ApiResourceMapping(typeof(Entities.Activity))]*/     Activity,
+        /*[ApiResponseMapping(typeof(Entities.Activity))]*/     ActivityListResponse,
+
+        /*[ApiResourceMapping(typeof(Entities.Caption))]*/      Caption,
+        /*[ApiResponseMapping(typeof(Entities.Caption))]*/      CaptionListResponse,
+
+        [ApiResourceMapping(typeof(Entities.Channel))]          Channel,
+        [ApiResponseMapping(typeof(Entities.Channel))]          ChannelListResponse,
+
+        /*[ApiResourceMapping(typeof(Entities.ChannelSection))]*/ChannelSection,
+        /*[ApiResponseMapping(typeof(Entities.ChannelSection))]*/ChannelSectionListResponse,
+
+        /*[ApiResourceMapping(typeof(Entities.Comment))]*/      Comment,
+        /*[ApiResponseMapping(typeof(Entities.Comment))]*/      CommentListResponse,
+
+        /*[ApiResourceMapping(typeof(Entities.CommentThread))]*/CommentThread,
+        /*[ApiResponseMapping(typeof(Entities.CommentThread))]*/CommentThreadListResponse,
+
+        /*[ApiResourceMapping(typeof(Entities.GuideCategory))]*/GuideCategory,
+        /*[ApiResponseMapping(typeof(Entities.GuideCategory))]*/GuideCategoryListResponse,
+
+        /*[ApiResourceMapping(typeof(Entities.I18nLanguage))]*/ I18nLanguage,
+        /*[ApiResponseMapping(typeof(Entities.I18nLanguage))]*/ I18nLanguageListResponse,
+
+        /*[ApiResourceMapping(typeof(Entities.I18nRegion))]*/   I18nRegion,
+        /*[ApiResponseMapping(typeof(Entities.I18nRegion))]*/   I18nRegionListResponse,
+
+        [ApiResourceMapping(typeof(Entities.Playlist))]         Playlist,
+        [ApiResponseMapping(typeof(Entities.PlaylistItem))]     PlaylistListResponse,
+
+        [ApiResourceMapping(typeof(Entities.PlaylistItem))]     PlaylistItem,
+        [ApiResponseMapping(typeof(Entities.PlaylistItem))]     PlaylistItemListResponse,
+
+        [ApiResourceMapping(typeof(Entities.SearchResult))]     SearchResult,
+        [ApiResponseMapping(typeof(Entities.SearchResult))]     SearchListResponse,
+
+        /*[ApiResourceMapping(typeof(Entities.Subscription))]*/ Subscription,
+        /*[ApiResponseMapping(typeof(Entities.Subscription))]*/ SubscriptionListResponse,
+
+        [ApiResourceMapping(typeof(Entities.Video))]            Video,
+        [ApiResponseMapping(typeof(Entities.Video))]            VideoListResponse,
+
+        /*[ApiResourceMapping(typeof(Entities.VideoCategory))]*/VideoCategory,
+        /*[ApiResponseMapping(typeof(Entities.VideoCategory))]*/VideoCategoryListResponse,
+    
     }
 }

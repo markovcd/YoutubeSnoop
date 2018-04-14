b62dd55cb0d432f4e4506844508fb6fe2f6009f2
Arkadiusz Markowski <markovcd@gmail.com>
2018-04-14 20:51:09 +0200

    Fluent api

--- Source/Fluent/_Base.cs
+++ Source/Fluent/_Base.cs
@@ -1,7 +1,31 @@
-﻿namespace YoutubeSnoop.Fluent
+﻿using YoutubeSnoop.Api;
+using YoutubeSnoop.Api.Entities;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace YoutubeSnoop.Fluent
 {
     public static partial class Youtube
     {
         public static int ResultsPerPage { get; set; } = 20;
+
+        public static IEnumerable<TItem> TakePages<TCollection, TItem, TResponse, TSettings>(this TCollection collection, int pageCount) 
+            where TCollection : YoutubeCollection<TItem, TResponse, TSettings>
+            where TItem : IYoutubeItem
+            where TResponse : class, IResponse
+            where TSettings : Settings
+        {
+            return collection.Take(collection.ResultsPerPage.GetValueOrDefault(20) * pageCount);
+        }
+
+        public static IEnumerable<TItem> TakePage<TCollection, TItem, TResponse, TSettings>(this TCollection collection)
+            where TCollection : YoutubeCollection<TItem, TResponse, TSettings>
+            where TItem : IYoutubeItem
+            where TResponse : class, IResponse
+            where TSettings : Settings
+        {
+            return collection.TakePages<TCollection, TItem, TResponse, TSettings>(1);
+        }
     }
 }
